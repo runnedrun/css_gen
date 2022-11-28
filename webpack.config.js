b084a015037e03b3a15a3dc297fe52dae56862f3
David Gaynor <runnedrun@gmail.com>
2022-11-28 18:28:56 +0100

    got the basic version working

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-  entry: './client.js',
-  mode: process.env.NODE_ENV || 'development',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
+  mode: "development",
+  entry: "./server.tsx",
+  target: "node",
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(ts|tsx|js)?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
       },
     ],
   },
-};
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    // fallback: {
+    //   fs: false,
+    //   tls: false,
+    //   net: false,
+    //   path: false,
+    //   zlib: false,
+    //   http: false,
+    //   https: false,
+    //   stream: false,
+    //   crypto: false,
+    //   buffer: false,
+    // },
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+}
